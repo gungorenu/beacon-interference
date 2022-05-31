b4a80a0ff63174c844aa817b08ab850f9eab1e49
UgurGungoren <ugur.gungoren@paynova.com>
2022-05-31 19:23:07 +0200

    first commit

---
+++ data-final-fixes.lua
@@ -0,0 +1,3 @@
+data.raw["beacon"]["beacon"].module_specification.module_slots = 4
+data.raw["beacon"]["beacon"].supply_area_distance = 5
+
---
+++ data.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = 'sprite',
+    name = 'bi-beacon-overload',
+    filename = '__Beacon-Interference__/graphics/bi-beacon-overload.png',
+    priority = 'extra-high-no-scale',
+    width = 64,
+    height = 64,
+    flags = {'icon'}
+  }
+})
